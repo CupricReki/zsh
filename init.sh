10c2ef064e4ba0a790756de7465b79754dd9f287
Skyler <cupric@laptop.ogbase.net>
2021-11-26 17:12:52 -0700

    update

--- init.sh
+++ init.sh
@@ -15,12 +15,13 @@ install_deb () {
 }
 
 dep_check () {
-# Checks for zsh, git, curl, fzy, and antibody
+	# Checks for zsh, git, curl, fzy, and antibody
 
 	type zsh >/dev/null 2>&1 || { deb_install "zsh"; }
 	type git >/dev/null 2>&1 || { deb_install "git"; }
 	type curl >/dev/null 2>&1 || { deb_install "curl"; }
 	type fzy >/dev/null 2>&1 || { deb_install "fzy"; }
+	type sqlite3 >/dev/null 2>&1 || { deb_install "sqlite3"; }
 	type antibody >/dev/null 2>&1 || { install_antibody; }
 }
 
@@ -39,9 +40,31 @@ install_antibody () {
 	curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 }
 
+check_zsh () {
+	# Checks to see if zsh is the default shell and offers to change it if it isn't
+	if [[ $(getent passwd $USER | awk -F: '{print $NF}') != "/bin/zsh" ]]; then
+		read -r -p "Change default shell to /bin/zsh? [Y/n] " input
+
+		case $input in
+		    [yY][eE][sS]|[yY])
+		 chsh -s /bin/zsh
+		 ;;
+		    [nN][oO]|[nN])
+		 echo "You can change the default shell to zsh using 'chsh -s /bin/zsh'"
+		       ;;
+		    *)
+		 echo "Invalid input..."
+		 exit 1
+		 ;;
+		esac
+
+	else
+		echo "zsh is already the default shell"
+	fi
+}
+
 dep_check
 get_zsh
+check_zsh
 
-cd ~
-#chsh -s /bin/zsh
-exec zsh
+echo 'reload shell or run exec zsh'
