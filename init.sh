53a47dc9a2d47846b3065a0dcb7b96cd7c19f329
cupric <cupricreki@gmail.com>
2022-05-15 00:10:40 -0400

    simplified

--- init.sh
+++ init.sh
@@ -40,26 +40,7 @@ install_antibody () {
 }
 
 check_zsh () {
-	# Checks to see if zsh is the default shell and offers to change it if it isn't
-	if [[ $(getent passwd $USER | awk -F: '{print $NF}') != "/bin/zsh" ]]; then
-		read -r -p "Change default shell to /bin/zsh? [Y/n] " input
-
-		case $input in
-		    [yY][eE][sS]|[yY])
-		 chsh -s /bin/zsh
-		 ;;
-		    [nN][oO]|[nN])
-		 echo "You can change the default shell to zsh using 'chsh -s /bin/zsh'"
-		       ;;
-		    *)
-		 echo "Invalid input..."
-		 exit 1
-		 ;;
-		esac
-
-	else
-		echo "zsh is already the default shell"
-	fi
+    chsh -s /bin/zsh
 }
 
 directory_clean () {
@@ -72,15 +53,8 @@ if [ "$1" = "clean" ]; then
 	directory_clean
 fi
 
-hierarchy () {
-	# Create folder and files to avoid issues on first creation with histdb
-	mkdir -p $HOME/.histdb
-	touch $HOME/.histdb/zsh-history.db
-}
-
 dep_check
 get_zsh
 check_zsh
-hierarchy
 zsh
 echo 'reload shell or run exec zsh'
