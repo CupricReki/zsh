54d2d1c46a285ff419f37ab2c6995ce883ca329f
cupric <CupricReki@gmail.com>
2024-01-20 14:38:30 -0500

    fix

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ dep_check () { # Checks for zsh, git, curl, fzf
 
 get_zsh () {
 	echo "Getting zsh configs from gitlab.ogbase.net/cupric/zsh.git"
-	git clone https://gitlab.ogbase.net/cupric/zsh.git $zshdir/zsh
+	git clone https://gitlab.ogbase.net/cupric/zsh.git $zshdir
 	rm -f $HOME/.zshrc
 	ln -s $zshdir/zshrc $HOME/.zshrc
     ln -s $zshdir/zshenv $HOME/.zshenv
