5f6169bdfdd746c548fc09a68f4112d6e3792a7c
cupric <CupricReki@gmail.com>
2024-09-14 10:34:23 -0400

    fixed sudo alias

--- custom/alias/alias.zsh
+++ custom/alias/alias.zsh
@@ -46,7 +46,7 @@ alias 6='cd -6'
 alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
-alias _='sudo '
+alias sudo='sudo '
 alias afind='ack -il'
 alias bwe='export BW_SESSION=$( bw unlock --raw )'
 alias cd=__enhancd::cd
